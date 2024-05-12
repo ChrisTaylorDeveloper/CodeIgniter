cc29264084564ac1805a41c94691d57875994ec0
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-05-12 11:31:44 +0100

    Make html in header look a bit more like the bootstrap tutorial.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,8 @@
-<html>
+<!doctype html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
 </head>
 <body>
