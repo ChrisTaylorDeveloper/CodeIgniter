922fd29ba62ce23dd1e22086ae95b110d9b6e1f0
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-05-12 11:38:14 +0100

    Add Bootstrap css and js.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     <h1>CodeIgniter 2 Tutorial</h1>
