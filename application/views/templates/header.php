7861d8481eddefafafc087fb5ac2b8dbec935bd2
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-05-12 11:55:27 +0100

    Nav bar: fix links and tidy up.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,20 +9,20 @@
 <body>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">CI2 Demos</a>
+        <a class="navbar-brand" href="/index.php/home">CI2 Demos</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="/index.php/home">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="/index.php/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    <a class="nav-link disabled" aria-disabled="true">Coming soon</a>
                 </li>
             </ul>
         </div>
