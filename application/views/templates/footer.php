cef1ef5c973a6e7f2541eded34391b25dd6ee62a
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-05-14 08:24:55 +0100

    Tweak text in footer.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,8 +2,7 @@
 </main>
 <footer class="footer mt-auto py-3">
   <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
-    <span class="text-muted"><strong>&copy; 2011</strong></span>
+    <span class="text-muted"><strong>&copy; 2024</strong></span>
   </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
